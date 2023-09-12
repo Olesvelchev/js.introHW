52a1f10e732c87ef69f0b3d61fc8f1f156c7d47d
Олесь Вельчев <138475823+Olesvelchev@users.noreply.github.com>
2023-09-12 13:38:25 +0000

    Виправленно 2 таску

--- Dom.06/02exercise.js
+++ Dom.06/02exercise.js
@@ -1,17 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var body = document.createElement('body');
-    var main = document.createElement('main');
-    main.className = 'mainClass check item';
+    var header = document.querySelector('h1');
+    header.style.color = 'white';
+    header.style.backgroundColor = 'green';
+    header.style.fontWeight = 'bold';
+    header.style.padding = '10px';
 
-    var div = document.createElement('div');
-    div.id = 'myDiv';
-
-    var paragraph = document.createElement('p');
-    paragraph.appendChild(document.createTextNode('First paragraph'));
-
-    div.appendChild(paragraph);
-    main.appendChild(div);
-    body.appendChild(main);
-
-    document.documentElement.appendChild(body);
-});
+    var paragraphs = document.querySelectorAll('p');
+    paragraphs[0].style.color = 'black';
+    paragraphs[1].style.color = 'black';
+    paragraphs[2].style.color = 'red';
+    paragraphs[3].style.color = 'black';
+    paragraphs[2].style.textDecoration = 'underline';
+    paragraphs[3].style.fontStyle = 'italic';
+    
+    var list = document.querySelector('#myList');
+    list.style.listStyleType = 'none';
+    
+    var spanElement = document.querySelector('span');
+    spanElement.style.display = 'none';
+  });
